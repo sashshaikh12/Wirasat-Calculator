1a8accbcec163abfdfb8d50997af3831ecb7f273
sashshaikh12 <sashshaikh12@gmail.com>
2025-06-01 07:53:07 +0530

    did proper setup enough to start coding now

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,15 @@
-import { Text, View } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
+import { Text } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-3xl text-red-600">Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <LinearGradient
+  colors={['#0D0B1F', '#6D28D9']}
+  className="flex-1"
+>
+  <Text className="text-white text-2xl font-semibold">Wirasat App</Text>
+</LinearGradient>
+
+
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
   content: ["./App.tsx", "./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '320px',   // Small phones
+        'sm': '375px',   // iPhone SE / mid phones
+        'md': '425px',   // Large phones
+        'lg': '768px',   // Small tablets
+        'xl': '1024px',  // Tablets / iPads
+      },
+    },
   },
   plugins: [],
 }
